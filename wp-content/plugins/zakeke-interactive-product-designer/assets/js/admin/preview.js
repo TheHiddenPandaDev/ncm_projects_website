a94688a3fb72e1c7078d72cd24360f6b9d5ebda1
Dani Roman <daniel.roman@thehiddenpanda.com>
2024-03-06 18:23:24 +0100

    NPD-29: Install plugins

--- wp-content/plugins/zakeke-interactive-product-designer/assets/js/admin/preview.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function () {
-	jQuery('.zakeke-preview').click(function (event) {
-		tb_show(this.lastElementChild.title, this.lastElementChild.src);
-		event.stopPropagation();
-	});
-});
--- wp-content/themes/goya-child/functions.php
+++ wp-content/themes/goya-child/functions.php
@@ -2,13 +2,13 @@
 /**
  * Child theme functions and definitions.
  */
-function goyagoya_child_enqueue_styles() {
-wp_enqueue_style( 'goya/goya-style' , get_template_directory_uri() . '/style.css' );    
-    wp_enqueue_style( 'goya/goya-child-style',
+function goya_child_enqueue_styles() {
+wp_enqueue_style( 'goya-style' , get_template_directory_uri() . '/style.css' );    
+    wp_enqueue_style( 'goya-child-style',
         get_stylesheet_directory_uri() . '/style.css',
-        array( 'goya/goya-style' ),
+        array( 'goya-style' ),
         1.0
     );
 }
 
-add_action(  'wp_enqueue_scripts', 'goyagoya_child_enqueue_styles', 99 );
+add_action(  'wp_enqueue_scripts', 'goya_child_enqueue_styles', 99 );
